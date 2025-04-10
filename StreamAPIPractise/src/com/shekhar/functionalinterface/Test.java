c9be28ac21a7900cd04301741e8ca1de784be814
HP <HP@192.168.10.96>
2025-04-10 17:44:57 +0530

    Third Commit

--- StreamAPIPractise/src/com/shekhar/functionalinterface/Test.java
+++ StreamAPIPractise/src/com/shekhar/functionalinterface/Test.java
@@ -10,6 +10,7 @@ public class Test {
 		int sum = add.sum(10, 20);
 		System.out.println(sum);
 
+		System.out.println("============+++++++++++++++=====");
 		Multiply mul = (a, b) -> a * b;
 		Double product = mul.product(10.00, 10.00);
 		System.out.println("Product Of This two:" + product);
