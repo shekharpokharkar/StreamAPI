b161706d05f370ee54ed18ec27eba271b910bb75
HP <HP@192.168.10.96>
2025-04-10 17:39:02 +0530

    Additing Multiplication Code to Git repo

---
+++ StreamAPIPractise/src/com/shekhar/functionalinterface/Multiply.java
@@ -0,0 +1,7 @@
+package com.shekhar.functionalinterface;
+
+public interface Multiply {
+
+	Double product(Double dNo, Double dNo2);
+
+}
--- StreamAPIPractise/src/com/shekhar/functionalinterface/Test.java
+++ StreamAPIPractise/src/com/shekhar/functionalinterface/Test.java
@@ -3,10 +3,15 @@ package com.shekhar.functionalinterface;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		
-		Addition add=(a,b)->{return a+b;};
-		int sum = add.sum(10,20);
+
+		Addition add = (a, b) -> {
+			return a + b;
+		};
+		int sum = add.sum(10, 20);
 		System.out.println(sum);
+
+		Multiply mul = (a, b) -> a * b;
+		Double product = mul.product(10.00, 10.00);
+		System.out.println("Product Of This two:" + product);
 	}
 }
